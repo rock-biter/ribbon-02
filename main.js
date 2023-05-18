6fae380adbf15748e9f36598c5f6aaacf1702bf2
rock-biter <74297920+rock-biter@users.noreply.github.com>
2023-05-18 23:13:06 +0200

    ADD - more ribbon with 3D simplex noise

--- main.js
+++ main.js
@@ -12,47 +12,94 @@ import {
 	MeshStandardMaterial,
 	SpotLightHelper,
 } from 'three'
-import { createNoise2D } from 'simplex-noise'
-const noise = createNoise2D()
+import { createNoise2D, createNoise3D } from 'simplex-noise'
+// const noise = createNoise2D()
+const noise = createNoise3D()
 console.log(noise)
 
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 const GLSL_SIMPLEX_NOISE = `
-vec3 permute(vec3 x) { return mod(((x*34.0)+1.0)*x, 289.0); }
+vec4 permute(vec4 x){return mod(((x*34.0)+1.0)*x, 289.0);}
+vec4 taylorInvSqrt(vec4 r){return 1.79284291400159 - 0.85373472095314 * r;}
 
-float snoise(vec2 v){
-  const vec4 C = vec4(0.211324865405187, 0.366025403784439,
-           -0.577350269189626, 0.024390243902439);
-  vec2 i  = floor(v + dot(v, C.yy) );
-  vec2 x0 = v -   i + dot(i, C.xx);
-  vec2 i1;
-  i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
-  vec4 x12 = x0.xyxy + C.xxzz;
-  x12.xy -= i1;
-  i = mod(i, 289.0);
-  vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
-  + i.x + vec3(0.0, i1.x, 1.0 ));
-  vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy),
-    dot(x12.zw,x12.zw)), 0.0);
-  m = m*m ;
-  m = m*m ;
-  vec3 x = 2.0 * fract(p * C.www) - 1.0;
-  vec3 h = abs(x) - 0.5;
-  vec3 ox = floor(x + 0.5);
-  vec3 a0 = x - ox;
-  m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
-  vec3 g;
-  g.x  = a0.x  * x0.x  + h.x  * x0.y;
-  g.yz = a0.yz * x12.xz + h.yz * x12.yw;
-  return 130.0 * dot(m, g);
+float snoise(vec3 v){ 
+  const vec2  C = vec2(1.0/6.0, 1.0/3.0) ;
+  const vec4  D = vec4(0.0, 0.5, 1.0, 2.0);
+
+// First corner
+  vec3 i  = floor(v + dot(v, C.yyy) );
+  vec3 x0 =   v - i + dot(i, C.xxx) ;
+
+// Other corners
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min( g.xyz, l.zxy );
+  vec3 i2 = max( g.xyz, l.zxy );
+
+  //  x0 = x0 - 0. + 0.0 * C 
+  vec3 x1 = x0 - i1 + 1.0 * C.xxx;
+  vec3 x2 = x0 - i2 + 2.0 * C.xxx;
+  vec3 x3 = x0 - 1. + 3.0 * C.xxx;
+
+// Permutations
+  i = mod(i, 289.0 ); 
+  vec4 p = permute( permute( permute( 
+             i.z + vec4(0.0, i1.z, i2.z, 1.0 ))
+           + i.y + vec4(0.0, i1.y, i2.y, 1.0 )) 
+           + i.x + vec4(0.0, i1.x, i2.x, 1.0 ));
+
+// Gradients
+// ( N*N points uniformly over a square, mapped onto an octahedron.)
+  float n_ = 1.0/7.0; // N=7
+  vec3  ns = n_ * D.wyz - D.xzx;
+
+  vec4 j = p - 49.0 * floor(p * ns.z *ns.z);  //  mod(p,N*N)
+
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_ );    // mod(j,N)
+
+  vec4 x = x_ *ns.x + ns.yyyy;
+  vec4 y = y_ *ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+
+  vec4 b0 = vec4( x.xy, y.xy );
+  vec4 b1 = vec4( x.zw, y.zw );
+
+  vec4 s0 = floor(b0)*2.0 + 1.0;
+  vec4 s1 = floor(b1)*2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+
+  vec4 a0 = b0.xzyw + s0.xzyw*sh.xxyy ;
+  vec4 a1 = b1.xzyw + s1.xzyw*sh.zzww ;
+
+  vec3 p0 = vec3(a0.xy,h.x);
+  vec3 p1 = vec3(a0.zw,h.y);
+  vec3 p2 = vec3(a1.xy,h.z);
+  vec3 p3 = vec3(a1.zw,h.w);
+
+//Normalise gradients
+  vec4 norm = taylorInvSqrt(vec4(dot(p0,p0), dot(p1,p1), dot(p2, p2), dot(p3,p3)));
+  p0 *= norm.x;
+  p1 *= norm.y;
+  p2 *= norm.z;
+  p3 *= norm.w;
+
+// Mix final noise value
+  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  m = m * m;
+  return 42.0 * dot( m*m, vec4( dot(p0,x0), dot(p1,x1), 
+                                dot(p2,x2), dot(p3,x3) ) );
 }
 `
 
 const scene = new Scene()
 
-const geometry = new BoxGeometry(14, 3, 0.1, 300, 180, 10)
-const material = new MeshStandardMaterial({ color: 0x452789 })
+const geometry = new BoxGeometry(20, 2, 0.1, 400, 200, 2)
+const material = new MeshStandardMaterial({
+	color: 0x452789,
+	wireframe: false,
+})
 
 const uniforms = {
 	uTime: { value: 0 },
@@ -85,15 +132,16 @@ material.onBeforeCompile = (shader) => {
 
 		#endif
 
-		float distance = mvPosition.x + 7.;
-		float noise = snoise( (mvPosition.xy + vec2(uTime * 0.005)) * vec2(0.25,0.06) ) * (0.4 + 0.1 * distance);
+		float distance = mvPosition.x ;
+		float noise = snoise( (mvPosition.xyx + vec3(uTime * 0.003)) * vec3(0.25,0.1,0.25) ) * (0.4 + 0.1);
+
+		mvPosition = modelMatrix * mvPosition;
 		
-		mvPosition.z += noise * smoothstep(0.,1.,(distance)*0.3);
-		// mvPosition.y -= (-snoise(mvPosition.xy + vec2(uTime * 0.001)) * 0.5 + distance*distance*0.1) * 0.1;
+		mvPosition.z += noise;
+		mvPosition.y += snoise( (mvPosition.xyz + vec3(uTime * 0.001)) * 0.1);
 		mvPosition.x -= distance * abs(noise) * 0.02 + distance * 0.08;
 
-		mvPosition = modelViewMatrix * mvPosition;
-
+		mvPosition = viewMatrix * mvPosition;
 		
 
 		gl_Position = projectionMatrix * mvPosition;
@@ -106,7 +154,14 @@ material.onBeforeCompile = (shader) => {
 const flag = new Mesh(geometry, material)
 flag.position.x = 0
 
-scene.add(flag)
+for (let i = 0; i < 10; i++) {
+	let clone = flag.clone()
+	clone.position.z += (i - 5) * 1
+
+	scene.add(clone)
+}
+
+// scene.add(flag)
 
 const camera = new PerspectiveCamera(
 	75,
@@ -133,7 +188,7 @@ document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
-controls.target = flag.position
+controls.target = new Vector3(0, 3, 0)
 
 function onResize() {
 	camera.aspect = window.innerWidth / window.innerHeight
